894205e87834fed565387f99e96ff904f2ca8585
Fugashi0119 <68821989+Fugashi0119@users.noreply.github.com>
2020-07-27 10:12:28 +0900

    ポートフォリオ

---
+++ node.js
@@ -0,0 +1,132 @@
+function popupImage1() {
+    var popup = document.getElementById('js-popup1');
+    if(!popup) return;
+  
+    var blackBg = document.getElementById('js-black-bg1');
+  
+    var blackBg = document.getElementById('js-black-bg1');
+    var closeBtn = document.getElementById('js-close-btn1');
+    var showBtn = document.getElementById('js-show-popup1');
+  
+    closePopUp(blackBg);
+    closePopUp(closeBtn);
+    closePopUp(showBtn);
+    function closePopUp(elem) {
+      if(!elem) return;
+      elem.addEventListener('click', function() {
+        popup.classList.toggle('is-show');
+      });
+    }
+}
+
+function popupImage2() {
+    var popup = document.getElementById('js-popup2');
+    if(!popup) return;
+  
+    var blackBg = document.getElementById('js-black-bg2');
+  
+    var blackBg = document.getElementById('js-black-bg2');
+    var closeBtn = document.getElementById('js-close-btn2');
+    var showBtn = document.getElementById('js-show-popup2');
+  
+    closePopUp(blackBg);
+    closePopUp(closeBtn);
+    closePopUp(showBtn);
+    function closePopUp(elem) {
+      if(!elem) return;
+      elem.addEventListener('click', function() {
+        popup.classList.toggle('is-show');
+      });
+    }
+}
+
+function popupImage3() {
+    var popup = document.getElementById('js-popup3');
+    if(!popup) return;
+  
+    var blackBg = document.getElementById('js-black-bg3');
+  
+    var blackBg = document.getElementById('js-black-bg3');
+    var closeBtn = document.getElementById('js-close-btn3');
+    var showBtn = document.getElementById('js-show-popup3');
+  
+    closePopUp(blackBg);
+    closePopUp(closeBtn);
+    closePopUp(showBtn);
+    function closePopUp(elem) {
+      if(!elem) return;
+      elem.addEventListener('click', function() {
+        popup.classList.toggle('is-show');
+      });
+    }
+}
+
+function popupImage4() {
+    var popup = document.getElementById('js-popup4');
+    if(!popup) return;
+  
+    var blackBg = document.getElementById('js-black-bg4');
+  
+    var blackBg = document.getElementById('js-black-bg4');
+    var closeBtn = document.getElementById('js-close-btn4');
+    var showBtn = document.getElementById('js-show-popup4');
+  
+    closePopUp(blackBg);
+    closePopUp(closeBtn);
+    closePopUp(showBtn);
+    function closePopUp(elem) {
+      if(!elem) return;
+      elem.addEventListener('click', function() {
+        popup.classList.toggle('is-show');
+      });
+    }
+}
+
+function popupImage5() {
+    var popup = document.getElementById('js-popup5');
+    if(!popup) return;
+  
+    var blackBg = document.getElementById('js-black-bg5');
+  
+    var blackBg = document.getElementById('js-black-bg5');
+    var closeBtn = document.getElementById('js-close-btn5');
+    var showBtn = document.getElementById('js-show-popup5');
+  
+    closePopUp(blackBg);
+    closePopUp(closeBtn);
+    closePopUp(showBtn);
+    function closePopUp(elem) {
+      if(!elem) return;
+      elem.addEventListener('click', function() {
+        popup.classList.toggle('is-show');
+      });
+    }
+}
+
+function popupImage6() {
+    var popup = document.getElementById('js-popup6');
+    if(!popup) return;
+  
+    var blackBg = document.getElementById('js-black-bg6');
+  
+    var blackBg = document.getElementById('js-black-bg6');
+    var closeBtn = document.getElementById('js-close-btn6');
+    var showBtn = document.getElementById('js-show-popup6');
+  
+    closePopUp(blackBg);
+    closePopUp(closeBtn);
+    closePopUp(showBtn);
+    function closePopUp(elem) {
+      if(!elem) return;
+      elem.addEventListener('click', function() {
+        popup.classList.toggle('is-show');
+      });
+    }
+}
+
+popupImage1();
+popupImage2();
+popupImage3();
+popupImage4();
+popupImage5();
+popupImage6();
